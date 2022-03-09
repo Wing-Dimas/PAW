32d7a7809e426c4f4d2a6a63dd78f3a0329deaf3
Wing-Dimas <wingdimas@gmail.com>
2022-03-09 09:34:22 +0700

    Menambahkan file fungsi.php

---
+++ Tugas 2/fungsi.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+INTRUCTIONS:
+    file fungsi.php berisi sebuah fungsi (bebas fungsi apa saja), 
+    kemudian lakukan pemanggilan terhadap fungsi tersebut minimal 3x
+*/
+
+function kuadrat($val){
+    return $val * $val;
+}
+
+echo kuadrat(20) . "<br>";
+echo kuadrat(4) . "<br>";
+echo kuadrat(12.5) . "<br>";
+echo kuadrat(-9) . "<br>";
+
+?>
