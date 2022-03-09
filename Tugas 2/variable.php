7131f7dace1fa50abd87e8a82964d6d85dcd2925
Wing-Dimas <wingdimas@gmail.com>
2022-03-09 09:21:21 +0700

    Menambahkan file variable.php

---
+++ Tugas 2/variable.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+INTRUCTIONS:
+    file variable.php berisi minimal 3 variable kemudian lakukan penjumlah, 
+    pengurungan dan perkalian dari variable variable tersebut. contoh $hasil = $a+$b;
+*/
+
+$angka1 = 15;
+$angka2 = 10;
+
+// operasi tambah
+$result = $angka1 + $angka2;
+echo  $angka1 . " + ". $angka2 . " = " . $result . "<br>";
+
+// operasi kurang
+$result = $angka1 - $angka2;
+echo  $angka1 . " - ". $angka2 . " = " . $result . "<br>";
+
+// operasi kali
+$result = $angka1 * $angka2;
+echo  $angka1 . " * ". $angka2 . " = " . $result . "<br>";
+
+// operasi bagi
+$result = $angka1 / $angka2;
+echo  $angka1 . " / ". $angka2 . " = " . $result . "<br>";
+
+// operasi modulus
+$result = $angka1 % $angka2;
+echo  $angka1 . " % ". $angka2 . " = " . $result . "<br>";
+
+
+?>
