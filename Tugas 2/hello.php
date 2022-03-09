dc3a8f1f7ee4c57564ff830c866d97f999e09c2a
Wing-Dimas <wingdimas@gmail.com>
2022-03-09 09:11:32 +0700

    Insiasia Tugas 2 dan Menambahkan file hello.php

---
+++ Tugas 2/hello.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+INTRUCTIONS:
+    file hello.php berisi code php sederhana yang menampilkan tulisan 
+    "Nama dan NIM anda masing-masing", sertakan commentar di dalam file php tersebut.    
+*/
+
+// Menampilkan nama mahasiswa
+echo "Nama : Dimas Wing Bagas Bimantara";
+// Membuat enter pada halaman HTML
+echo "<br>";
+// Menampilkan NIM mahasiswa
+echo "NIM  : 200411100119"
+
+?>
